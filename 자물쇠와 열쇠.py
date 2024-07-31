93269e8dbff7bfa859d35dcce3e7a56845f2e39e
[justpers] <[nunajiyo@naver.com]>
2024-07-31 19:05:15 +0900

    키와 자물쇠 비교를 반복문 안에서 해서, 같거나 틀린게 나왔을 때 바로 반복문이 끝나는 문제 해결함

--- 자물쇠와 열쇠.py
+++ 자물쇠와 열쇠.py
@@ -1,40 +1,42 @@
 def rotation(key):
     m = len(key[0])
-    rot_arr = [[0] * m] * m
+    rot_arr = [[0] * m for _ in range(m)]
     for i in range(m):
         for j in range(m):
             rot_arr[i][j] = key[m-j-1][i]
     return rot_arr
-    
+
+def check(new_lock, n):
+    for i in range(n, 2 * n):
+        for j in range(n, 2 * n):
+            if new_lock[i][j] != 1:
+                return False
+    return True
+ 
 def solution(key, lock):
     
     m = len(key[0])
     n = len(lock[0])
     
     # 수월하게 탐색하기 위해 lock 크기 3배로 늘리고 0으로 채우기
-    new_lock = [[0] * n * 3] * n * 3
+    new_lock = [[0] * (n * 3) for _ in range(n*3)]
     for i in range(n, 2*n):
         for j in range(n, 2*n):
             new_lock[i][j] = lock[i-n][j-n]
             
     for _ in range(4):
         key = rotation(key) # 오른쪽으로 90도
-        for col in range(n*3 - n +1):
-            for row in range(n*3-n+1):
+        for row in range(n*3 - n +1):
+            for col in range(n*3-n+1):
                 for i in range(m):
                     for j in range(m):
-                        new_lock[col + i][row + j] += key[i][j]
+                        new_lock[row + i][col + j] += key[i][j]
                 
-                for i in range(n, n*2):
-                    for j in range(n, n*2):
-                        if new_lock[i][j] == 1:
-                            return True
+                if check(new_lock, n):
+                    return True
+                
                 for i in range(m):
                     for j in range(m):
-                        new_lock[col + i][row + j] -= key[i][j]
+                        new_lock[row + i][col + j] -= key[i][j]
 
     return False
-
-key = [[0,0,0],[1,0,0],[0,1,1]]
-lock = [[1,1,1],[1,1,0], [1,0,1]]
-solution(key, lock)
