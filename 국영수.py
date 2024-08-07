ea060b63fbbb180a5c6267710d4826b9662dbfc2
[justpers] <[nunajiyo@naver.com]>
2024-08-07 21:22:17 +0900

    정렬 조건에 문자열 조건도 넣음 -> 정답

--- 국영수.py
+++ 국영수.py
@@ -4,7 +4,7 @@ for _ in range(n):
     data = input().split()
     lst.append((data[0], int(data[1]), int(data[2]), int(data[3])))
 
-lst = sorted(lst, key = lambda x: (-x[1], x[2], -x[3]))
+lst = sorted(lst, key = lambda x: (-x[1], x[2], -x[3], x[0]))
 
 for i in lst:
     print(i[0])
