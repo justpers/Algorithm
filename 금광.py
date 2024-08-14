a938f7e86067b0bded7d17946c859f20d0cb68d1
[justpers] <[nunajiyo@naver.com]>
2024-08-14 19:01:12 +0900

    두 번째 시도. 인덱스 수정, 테스트 케이스 여러 개 받을 수 있도록 수정

--- 금광.py
+++ 금광.py
@@ -1,30 +1,33 @@
 t = int(input())
-n, m = map(int, input().split()) # 금광 크기
-gold_lst = list(map(int, input().split())) # 금 개수
-gold_map = []
-index = 0
-for i in range(n):
-    gold_map.append(gold_lst[index:index+m])
-    index += m
+while t > 0:
+    
+    n, m = map(int, input().split()) # 금광 크기
+    gold_lst = list(map(int, input().split())) # 금 개수
+    gold_map = []
+    index = 0
+    for i in range(n):
+        gold_map.append(gold_lst[index:index+m])
+        index += m
 
-# 왼쪽으로는 다시 돌아갈 수 없음 -> 1번째 행에서의 선택 먼저 고려
-for i in range(1, m):
-    for j in range(n):
-        if i == n - 1:
-            left_down = 0
-        else:
-            left_down = gold_map[i+1][j-1]
+    # 왼쪽으로는 다시 돌아갈 수 없음 -> 1번째 행에서의 선택 먼저 고려
+    for i in range(1, m):
+        for j in range(n):
+            if j == n - 1:
+                left_down = 0
+            else:
+                left_down = gold_map[j+1][i-1]
+                
+            left_str = gold_map[j][i-1]
             
-        left_str = gold_map[i][j-1]
-        
-        if j == 0:
-            left_up = 0
-        else:
-            left_up = gold_map[i-1][j-1]
+            if j == 0:
+                left_up = 0
+            else:
+                left_up = gold_map[j-1][i-1]
 
-        gold_map[i][j] = gold_map[i][j] + max(left_down, left_str, left_up)
+            gold_map[j][i] = gold_map[j][i] + max(left_down, left_str, left_up)
 
-max_value = 0
-for i in range(n):
-    max_value = max(max_value, gold_map[i][m-1])
-print(max_value)
+    max_value = 0
+    for i in range(n):
+        max_value = max(max_value, gold_map[i][m-1])
+    print(max_value)
+    t -= 1
