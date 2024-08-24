273d7f418e1ba69d716f8c1fef2b8bf5cb9ed050
[justpers] <[nunajiyo@naver.com]>
2024-08-24 21:44:18 +0900

    파일 이름 변경

--- 숫자 카드 게임.py
+++
@@ -1,8 +0,0 @@
-n, m = map(int, input().split())
-
-result = 0
-for i in range(n):
-    row = list(map(int, input().split()))
-    min_value = min(row)
-    result = max(result, min_value)
-print(result)
--- 위에서 아래로.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-lst = []
-for i in range(n):
-    lst.append(int(input()))
-    
-array = sorted(lst, reverse=True)
-for i in array:
-    print(i, end=' ')
