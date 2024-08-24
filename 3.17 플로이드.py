9c1a0a2fd14ac8efc72293e3600268754c2f59ba
[justpers] <[nunajiyo@naver.com]>
2024-08-25 02:34:46 +0900

    플로이드 워셜 알고리즘 사용

---
+++ 3.17 플로이드.py
@@ -0,0 +1,35 @@
+INF = int(1e9)
+n = int(input())
+m = int(input())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if a == b:
+            graph[a][b] = 0
+
+for _ in range(m):
+    # A에서 B로 가는 비용이 C
+    a, b, c = map(int, input().split())
+    if c < graph[a][b]:
+        graph[a][b] = c
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            
+# 점화식에 따라 플로이드 워셜 알고리즘을 수행
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+            
+# 수행된 결과 출력
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[a][b], end = ' ')
+    print()
